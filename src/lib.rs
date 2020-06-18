4d0f4376bde580990158afdcd1bcc025ec9f2c39
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-06-19 02:34:23 +0800

    Implemented call_via_deref, plus impl_trait_for_boxed and friends

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,60 @@ macro_rules! visit_members {
     ($callback:ident;) => {};
 }
 
+#[macro_export]
+macro_rules! call_via_deref {
+    (
+        $( #[$attr:meta] )*
+        fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty )*) $(-> $ret:ty)?
+    ) => {
+        fn $name(&self $(, $arg_name : $arg_ty )*) $(-> $ret)? {
+            (**self).$name( $($arg_name),* )
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_trait_for_boxed {
+    (
+        $( #[$attr:meta] )*
+        $vis:vis trait $name:ident {
+            $( $body:tt )*
+        }
+    ) => {
+        impl<F: $name + ?Sized> $name for Box<F> {
+            visit_members!( call_via_deref; $($body)* );
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_trait_for_ref {
+    (
+        $( #[$attr:meta] )*
+        $vis:vis trait $name:ident {
+            $( $body:tt )*
+        }
+    ) => {
+        impl<'f, F: $name + ?Sized> $name for &'f dyn F {
+            visit_members!( call_via_deref; $($body)* );
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_trait_for_mut_ref {
+    (
+        $( #[$attr:meta] )*
+        $vis:vis trait $name:ident {
+            $( $body:tt )*
+        }
+    ) => {
+        impl<'f, F: $name + ?Sized> $name for &'f mut dyn F {
+            visit_members!( call_via_deref; $($body)* );
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +115,52 @@ mod tests {
             fn get_x(&self) -> u32;
             fn get_y(&self) -> u32;
         }
+    }
+
+    #[test]
+    fn defer_to_item_behind_pointer() {
+        trait GetX {
+            fn get_x(&self) -> u32;
+        }
+
+        impl GetX for u32 {
+            fn get_x(&self) -> u32 { *self }
+        }
+
+        impl GetX for Box<u32> {
+            call_via_deref!( fn get_x(&self) -> u32 );
+        }
+
+        fn assert_is_get_x<G: GetX>() {}
+
+        assert_is_get_x::<u32>();
+        assert_is_get_x::<Box<u32>>();
+    }
+
+    #[test]
+    fn impl_trait_for_boxed() {
+        trait Foo {
+            fn get_x(&self) -> u32;
+            fn execute(&self, expression: &str);
+        }
+
+        impl Foo for u32 {
+            fn get_x(&self) -> u32 { unimplemented!() }
+
+            fn execute(&self, _expression: &str) { unimplemented!() }
+        }
+
+        impl_trait_for_boxed! {
+            trait Foo {
+                fn get_x(&self) -> u32;
+                fn execute(&self, expression: &str);
+            }
+        }
+
+        fn assert_is_foo<F: Foo>() {}
+
+        assert_is_foo::<u32>();
+        assert_is_foo::<Box<u32>>();
+        assert_is_foo::<Box<dyn Foo>>();
     }
 }
